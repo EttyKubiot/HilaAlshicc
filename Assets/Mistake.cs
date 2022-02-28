ec95580c29c65982c3ccccc6a2c85bbd28785226
EttyBlack <6166887@gmail.com>
2022-02-28 14:40:58 +0200

    1

--- Assets/Mistake.cs
+++ Assets/Mistake.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Draw : MonoBehaviour
+public class Mistake : MonoBehaviour
 {
-//#if UNITY_EDITOR
+    //#if UNITY_EDITOR
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if(hit.collider != null)
+                if (hit.collider != null)
                 {
-                    Color newColor = Color.blue;
-                   
-                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
-                   
+                    Color newColor2 = Color.red;
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor2;
                 }
             }
         }
