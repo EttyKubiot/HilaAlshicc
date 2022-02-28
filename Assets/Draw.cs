7955babc4d09702091f6df3ed9cbcd1f2b6ea45c
EttyBlack <6166887@gmail.com>
2022-02-28 12:41:58 +0200

    Begin code Editor

---
+++ Assets/Draw.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+//#if UNITY_EDITOR
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null)
+                {
+                    Color newColor = Color.blue;
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+                }
+            }
+        }
+    }
+}
