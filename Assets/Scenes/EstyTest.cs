751f5a7754f9e7b0ae4141a57c5377dd76d0eb87
ESTY-PORGES <ep0583217291@gmail.com>
2022-02-28 15:49:17 +0200

    try lineRender

---
+++ Assets/Scenes/EstyTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstyTest : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    [SerializeField] private EstyLine line;
+
+    private void Start()
+    {
+        line.SetUpLine(points);
+    }
+}
