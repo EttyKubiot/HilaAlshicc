af75260e8d3be5bb6161e0a0e442d25baab8ffec
EttyBlack <6166887@gmail.com>
2022-02-28 22:55:30 +0200

    2

---
+++ Assets/Scenes/LineScale.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LineScale : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+
+    private Vector2 startPosition;
+    private float scalingFactor = .01f;
+    private bool isObjectAlreadyScaled = false;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = Vector2.zero;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!isObjectAlreadyScaled)
+        {
+            startPosition += eventData.delta;
+            var scalingAmount = new Vector2(Mathf.Abs(startPosition.x), Mathf.Abs(startPosition.y));
+            transform.localScale = (Vector3)scalingAmount * scalingFactor;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isObjectAlreadyScaled = true;
+    }
+}
